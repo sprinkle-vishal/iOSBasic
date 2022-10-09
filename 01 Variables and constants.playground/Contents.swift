d09ac6c1be7e394a2923658e20166c5975adb38a
sprinkle-vishal <vishal.bhola@sprinkledata.com>
2022-10-09 18:26:35 +0530

    Variables and Constants

---
+++ 01 Variables and constants.playground/Contents.swift
@@ -0,0 +1,12 @@
+var username = "Vishal"
+let accountId = 123456789
+
+username = "Vishal Bhola"
+//accountId = 124112 // when let is used, it considered that variable as constant, and we can't change the value of that.
+
+var numberOfLoginInADay: Int = 34
+
+numberOfLoginInADay = 12
+
+var name, email, address: String //All these variables are of String types
+
